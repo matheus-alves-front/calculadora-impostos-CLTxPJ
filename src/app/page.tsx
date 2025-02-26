2711b5546c83732c69c47ec5c2f2babea5193b18
matheus alves <matheus.alves.front.m2@gmail.com>
2025-02-26 06:17:17 -0300

    meta data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,34 @@
 import SalaryCalculator from "@/components/Calculator";
 import Script from "next/script";
 
+export const metadata = {
+  title: 'CNPJ x CLT Calculadora',
+  description: 'Esta é uma página destinada a quem quer saber a diferença de descontos do governo de cada modelo de contratação.',
+  keywords: ['CNPJ', 'CLT', 'salário', 'calculadora imposto', 'imposto'],
+  authors: [{ name: 'Matheus Alves' }],
+  openGraph: {
+    title: 'CNPJ x CLT Calculadora',
+    description: 'Esta é uma página destinada a quem quer saber a diferença de descontos do governo de cada modelo de contratação.',
+    url: 'https://calculadoraimpostoscltxpj.vercel.app/',
+    siteName: 'CNPJ x CLT Calculadora',
+    images: [
+      {
+        url: 'https://calculadoraimpostoscltxpj.vercel.app/haddad.webp',
+        width: 300,
+        height: 320,
+      },
+    ],
+    locale: 'pt_BR',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'CNPJ x CLT Calculadora',
+    description: 'Esta é uma página destinada a quem quer saber a diferença de descontos do governo de cada modelo de contratação.',
+    images: ['https://calculadoraimpostoscltxpj.vercel.app/haddad.webp'],
+  },
+};
+
 export default function Home() {
   return (
     <main className="bg-background relative">
